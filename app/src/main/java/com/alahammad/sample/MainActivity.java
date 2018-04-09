24845da56f1a6eb5af75fe5f0a12251c7393980d
Ala Hammad <alaa-trycarriage@trycarriage.com>
2018-04-09 17:04:20 +0300

    add OtpView to sample app

--- app/src/main/java/com/alahammad/sample/MainActivity.java
+++ app/src/main/java/com/alahammad/sample/MainActivity.java
@@ -3,11 +3,16 @@ package com.alahammad.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alahammad.otp_view.OtpView;
+
 public class MainActivity extends AppCompatActivity {
+    OtpView otpView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        otpView = findViewById(R.id.otp);
+//        otpView.enableKeypad();
     }
 }
